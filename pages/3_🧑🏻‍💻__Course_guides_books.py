66db0210b8192080132eeecc3608c5d9da55bc21
Wolf1904 <123803729+Wolf1904@users.noreply.github.com>
2023-04-12 20:09:54 +0530

    Update 5.5

---
+++ pages/3_🧑🏻‍💻__Course_guides_books.py
@@ -0,0 +1,73 @@
+import streamlit as st
+from PIL import Image
+from streamlit.components.v1 import html
+import time
+
+st.set_page_config(page_title="Practical Books", page_icon="🧑🏻‍💻", layout="wide")
+
+def local_CSS(file_name):
+    with open(file_name) as f:
+        st.markdown(f"<style>{f.read()}</style>", unsafe_allow_html=True)
+
+local_CSS("style/style1.css")
+
+
+st.header("Course Guides")
+st.write("These guides helps you to gain the knowledge of the particular course.")
+st.write('---')
+
+
+st.subheader("Course Guide Books:")
+
+with st.container():
+    st.write("###")
+    image_column, text_column = st.columns((1, 4))
+    with image_column:
+        st.image("images/pwk.jpg")
+    with text_column:
+        st.subheader("PWK (PEN-200) - Official OffSec Course")
+        st.text("Author: Offensive Security")
+        st.write(
+            """
+            PWK (PEN-200) ethical hacking course overhaul: 33% more lab machines, double the content. PWK is a penetration testing certification designed by the minds behind Kali Linux. Real-world Benefits. Interactive Exercises. Active Student Forums. Industry-leading Training.
+            """
+            )
+        button = st.button('*Generate link*', key = "1")
+        if button:
+            link = 'https://drive.google.com/file/d/1Xia2KtYrpyDqQ8jCJ73sW3pr9jYRoThS/view?usp=share_link'
+            st.write('*Generating link...*')
+            time.sleep(2)  # Simulate some processing time
+            hyperlink = f'<a href="{link}" target="_blank">Click here to read eBook...</a>'
+            html(hyperlink, height=30)
+
+with st.container():
+    st.write("###")
+    image_column, text_column = st.columns((1, 4))
+    with image_column:
+        st.image("images/ccna.jpg")
+    with text_column:
+        st.subheader("Cisco CCNA: Cisco Certified Network Associate (200-301)")
+        st.text("Author: Cisco")
+        st.write(
+            """
+            These courses cover network fundamentals, network access, IP connectivity, IP services, network security fundamentals, and laying the foundation for network automation and programmability.
+            """
+            )
+        left_column, right_column = st.columns((2))
+        with left_column:
+            button = st.button('*Volume 1*', key = "2")
+            if button:
+                link = 'https://drive.google.com/file/d/1s2IlR0gVs4P4SRix0NUTbpJPGFTonQB2/view?usp=share_link'
+                st.write('*Generating link...*')
+                time.sleep(2)  # Simulate some processing time
+                hyperlink = f'<a href="{link}" target="_blank">Click here to read eBook...</a>'
+                html(hyperlink, height=30)
+        with right_column:
+            button = st.button('*Volume 2*', key = "3")
+            if button:
+                link = 'https://drive.google.com/file/d/1NsXfc4xY0MgdIaUFY-2qraeCorUvgEZJ/view?usp=share_link'
+                st.write('*Generating link...*')
+                time.sleep(2)  # Simulate some processing time
+                hyperlink = f'<a href="{link}" target="_blank">Click here to read eBook...</a>'
+                html(hyperlink, height=30)
+
